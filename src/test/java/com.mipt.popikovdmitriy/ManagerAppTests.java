a2506db3e3d368fa23480af5410f6915fd43fd04
PopikovDmitriy <popikov.ds@phystech.edu>
2026-02-28 12:23:34 +0300

    Инициализация и настройка проекта

---
+++ src/test/java/com.mipt.popikovdmitriy/ManagerAppTests.java
@@ -0,0 +1,13 @@
+package com.mipt.popikovdmitriy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
